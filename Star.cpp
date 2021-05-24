a5e663997d097d4e6d2130fab34516504eedc467
H3lsinki <44325474+jmmaheswar@users.noreply.github.com>
2021-05-24 12:15:17 +0530

    Create Star.cpp
    Program to draw a star using Turbo C

---
+++ Star.cpp
@@ -0,0 +1,22 @@
+#include<conio.h>
+#include<stdio.h>
+#include<graphics.h>
+  void main()
+   {
+    int gd = DETECT, gm ;
+    initgraph(&gd,&gm,"C:\\Turboc3\\BGI");
+
+    setfillstyle(1,14);
+    line(300,65,150,275);
+    line(300,65,450,275);
+    line(150,275,450,275);
+    floodfill(305,105,15);
+
+    line(150,145,300,360);
+    line(150,145,450,145);
+    line(300,360,450,145);
+    floodfill(195,185,15);
+    floodfill(395,185,15);
+    floodfill(305,350,15);
+    getch();
+     }
